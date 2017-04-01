d00f0367de2298312e9ed4cf365b7395767408f9
mahesh kumar <mahesh9254@gmail.com>
2017-04-01 21:44:44 +0530

    snippet

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,13 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+   if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            // TODO: write your implementation of Equals() here
+            throw new NotImplementedException();
+            return base.Equals(obj);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            throw new NotImplementedException();
+            return base.GetHashCode();
+        }
         public ActionResult Index()
         {
+            do
+            {
+
+            } while (true);
+            while (true)
+            {
+
+            }
+           
+            try
+            {
+
+            }
+            finally
+            {
+
+            }
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
             return View();
         }
 
